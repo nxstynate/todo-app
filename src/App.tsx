0f109a678756ad36a218ff05612b24203b6df1d5
Nate Lewis <nathanael.paul.lewis@gmail.com>
2023-05-02 01:07:48 -0500

    Added some notes on the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import "./App.css";
 import InputBoxTest from "./components/InputBoxTest";
 import Title from "./components/Title";
 
+// Add delete functionality
+// Seperate the add button from the add list item functionality.
+
 export default function App() {
   return (
     <>
